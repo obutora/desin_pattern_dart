79d41ac30baa346d0f9d34d91b3205ba3f98e3f7
chan haga <obutora@gmail.com>
2023-06-16 23:13:05 +0900

    init

---
+++ bin/design_pattern_dart.dart
@@ -0,0 +1,5 @@
+import 'package:design_pattern_dart/design_pattern_dart.dart' as design_pattern_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${design_pattern_dart.calculate()}!');
+}
